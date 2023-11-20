285892f893aae410aaf852fba8d1e1975dbf111f
AlessandroArtusa <artusa@usi.ch>
2023-11-20 17:06:58 +0100

    created method sortOrderedArrayInt

--- exp01/Experiment.java
+++ exp01/Experiment.java
@@ -1,4 +1,5 @@
 package exp01;
+
 import java.util.Random;
 
 // import exp01.BubbleSortPassPerItem;
@@ -12,30 +13,30 @@ public class Experiment {
         int length3 = 1000;
         int length4 = 10000;
         Integer[] RNumbers = generateRandomArray(length1);
-     
+
         BubbleSortPassPerItem<Integer> sorter = new BubbleSortPassPerItem<>();
-       
+
         long startTime;
         long endTime;
-        
-        warmUpJVM(sorter, length1, 1000);
-        System.err.println("Warmup done!");
-        for(int i = 0; i<30; i++){
-            startTime = System.nanoTime();
-            sorter.sort(RNumbers);
-            endTime = System.nanoTime();
-            long duration = endTime - startTime;
-            System.out.println("Time taken to sort: " + duration + " nanoseconds");
 
-        }
+        // warmUpJVM(sorter, length1, 1000);
+        // System.err.println("Warmup done!");
+        // for (int i = 0; i < 30; i++) {
+        //     startTime = System.nanoTime();
+        //     sorter.sort(RNumbers);
+        //     endTime = System.nanoTime();
+        //     long duration = endTime - startTime;
+        //     System.out.println("Time taken to sort: " + duration + " nanoseconds");
 
-    
+        // }
+
+        sortOrderedArrayInt(length3, length3);
+
     }
 
-    private static void warmUpJVM(BubbleSortPassPerItem<Integer> sorter, int length, int iterations) {
+    private static void warmUpJVM(Sorter sorter, int length, int iterations) {
         for (int i = 0; i < iterations; i++) {
             Integer[] dummyArray = generateRandomArray(length);
-            System.out.println("Array sorted "+ i);
             sorter.sort(dummyArray);
         }
     }
@@ -51,5 +52,56 @@ public class Experiment {
         return array;
     }
 
-    
+    private static void sortOrderedArrayInt(int arrayLength, int iterations) {
+        Integer[] orderedArray = new Integer[arrayLength];
+        for (int i = 0; i < arrayLength; i++) {
+            orderedArray[i] = i;
+        }
+
+        BubbleSortPassPerItem<Integer> bubbleSortPassPerItem = new BubbleSortPassPerItem<>();
+        BubbleSortUntilNoChange<Integer> bubbleSortUntilNoChange = new BubbleSortUntilNoChange<>();
+        BubbleSortWhileNeeded<Integer> bubbleSortWhileNeeded = new BubbleSortWhileNeeded<>();
+
+        long totalTimePassPerItem = 0;
+
+        // BubbleSortPassPerItem
+        warmUpJVM(bubbleSortPassPerItem, arrayLength, iterations);
+        for (int i = 0; i < iterations; i++) {
+            long startTime = System.nanoTime();
+            bubbleSortPassPerItem.sort(orderedArray);
+            long endTime = System.nanoTime();
+            long duration = endTime - startTime;
+            System.out.println("Time taken to sort with BubbleSortPassPerItem: " + duration + " nanoseconds");
+            totalTimePassPerItem += duration;
+        }
+
+
+        // BubbleSortUntilNoChange
+        long totalTimeUntilNoChange = 0;
+        warmUpJVM(bubbleSortUntilNoChange, arrayLength, iterations);
+        for (int i = 0; i < iterations; i++) {
+            long startTime = System.nanoTime();
+            bubbleSortUntilNoChange.sort(orderedArray);
+            long endTime = System.nanoTime();
+            long duration = endTime - startTime;
+            System.out.println("Time taken to sort with BubbleSortUntilNoChange: " + duration + " nanoseconds");
+            totalTimeUntilNoChange += duration;
+        }
+
+        // BubbleSortWhileNeeded
+        long totalTimeWhileNeeded = 0;
+        warmUpJVM(bubbleSortWhileNeeded, arrayLength, iterations);
+        for (int i = 0; i < iterations; i++) {
+            long startTime = System.nanoTime();
+            bubbleSortWhileNeeded.sort(orderedArray);
+            long endTime = System.nanoTime();
+            long duration = endTime - startTime;
+            System.out.println("Time taken to sort with BubbleSortWhileNeeded: " + duration + " nanoseconds");
+            totalTimeWhileNeeded += duration;
+        }
+        
+        System.out.println("Average time taken to sort with BubbleSortPassPerItem: " + totalTimePassPerItem / iterations + " nanoseconds");
+        System.out.println("Average time taken to sort with BubbleSortUntilNoChange: " + totalTimeUntilNoChange / iterations + " nanoseconds");
+        System.out.println("Average time taken to sort with BubbleSortWhileNeeded: " + totalTimeWhileNeeded / iterations + " nanoseconds");
+    }
 }
